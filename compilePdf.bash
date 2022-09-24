ff1f3040371846dc70f3f14c8f4ba957bb10b3a3
scastd00 <scastd00@estudiantes.unileon.es>
2022-09-24 20:23:18 +0200

    Fixed problems with cites and code is shown in document. NOTE: Do not compile from IntelliJ

--- compilePdf.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pdflatex --shell-escape main.tex
-mv main.{log,aux,lof,lot,toc} out
-evince main.pdf &
---
+++ src/compilePdf.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Compile the document
+pdflatex -shell-escape main.tex
+
+# Compile the bibliography
+bibtex main
+
+# Compile the document again to include the bibliography references correctly
+# If the document is not compiled twice '?' characters are shown in the cites instead of the number of the reference
+pdflatex -shell-escape main.tex
+pdflatex -shell-escape main.tex
+
+mv main.{log,aux,bbl,blg,lof,lot,toc,pdf} ../out
